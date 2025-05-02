b16be0e4fb4db452f3de51673e638682b6324417
Vaibhav Arora <aroravaibhav817@gmail.com>
2025-05-03 01:02:31 +0530

    add cors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use actix_cors::Cors;
 use actix_web::{App, HttpServer};
 
 use config::ClankerConfig;
@@ -10,16 +11,25 @@ pub mod services;
 pub mod tx_utils;
 pub mod update;
 
-#[tokio::main]
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
     let config = ClankerConfig::get_config();
 
-    return HttpServer::new(|| App::new().service(update_pool))
-        .bind(("localhost", config.port))?
-        .run()
-        .await;
+    return HttpServer::new(|| {
+        App::new()
+            .wrap(
+                Cors::default()
+                    .allow_any_origin()
+                    .allowed_methods(vec!["POST"])
+                    .allowed_headers(vec!["Content-Type"]),
+            )
+            .service(update_pool)
+    })
+    .bind(("localhost", config.port))?
+    .run()
+    .await;
 }
 
 // cargo watch -c -w src -x run
