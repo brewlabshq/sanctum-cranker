ae522d35e52aa157a952531c486620886e647c71
Vaibhav Arora <82141868+VaibhavArora19@users.noreply.github.com>
2025-05-12 16:43:04 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use actix_web::{App, HttpServer};
 
 use config::ClankerConfig;
 use dotenv::dotenv;
-use routes::clanker_route::{hello_clanker, update_pool};
+use routes::clanker_route::{health_check, hello_clanker, update_pool};
 
 pub mod config;
 pub mod routes;
@@ -29,6 +29,7 @@ async fn main() -> std::io::Result<()> {
             )
             .service(update_pool)
             .service(hello_clanker)
+            .service(health_check)
     })
     .bind(("0.0.0.0", config.port))?
     .run()
